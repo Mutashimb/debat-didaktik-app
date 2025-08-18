263b63d9cc1fc183b506b7e32413889912129362
Hanifan Mutashim Billah <hanifanmutasim@gmail.com>
2025-08-18 08:07:07 +0700

    a whole changes in settings.py

--- api/config/settings.py
+++ api/config/settings.py
@@ -137,6 +137,6 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
